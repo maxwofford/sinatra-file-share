3c4cb6f5b2dfa476a32fed8aa4c91b8ced8b4e9a
Max Wofford <max.speed.wofford@gmail.com>
2014-09-30 22:31:20 -0600

    files upload to files directory

--- server.rb
+++ server.rb
@@ -3,12 +3,6 @@ require "sinatra/activerecord"
 require './environments'
 require 'haml'
 
-# Models
-
-class Document < ActiveRecord::Base
-
-end
-
 # Views
 
 get '/' do
@@ -20,6 +14,7 @@ get '/download/:filename' do |filename|
 	send_file "./files/#{filename}", :filename => filename, :type => 'Application/octet-stream'
 end
 
+
 # Uploads
 get '/upload' do
 	haml :upload
@@ -27,7 +22,7 @@ end
 
 get '/upload/list' do
 	@base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
-	@list = Dir.glob("./uploads/*.*").map{|f| f.split('/').last}
+	@list = Dir.glob("./files/*.*").map{|f| f.split('/').last}
 	haml :list
 end
 
@@ -35,12 +30,12 @@ post '/upload' do
 	filename = params['myfile'][:filename]
 	
 	# Check uniqueness of file name
-	if Dir.glob("./uploads/*.*").map{|f| f.split('/').last}.include?(filename)
+	if Dir.glob("./files/*.*").map{|f| f.split('/').last}.include?(filename)
 		filename = filename + "(#{rand(1000)})"
 	end
 
 	# Download the file
-	File.open('uploads/' + filename, "w") do |f|
+	File.open('files/' + filename, "w") do |f|
 		f.write(params['myfile'][:tempfile].read)
 	end
 	return "File successfully uploaded"
